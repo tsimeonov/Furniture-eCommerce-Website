bcefcb9865fc728035f3c7db8e12bacef261e6b4
tsimeonov <tsimeonov12@gmail.com>
2024-07-05 14:24:27 +0200

    create the toggleNavbar variable

--- app.js
+++ app.js
@@ -18,3 +18,9 @@ const navbar = document.querySelector("[data-navbar]");
 const navbarLinks = document.querySelectorAll("[data-nav-link]");
 const navTogglers = document.querySelectorAll("[data-nav-toggler]");
 const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("active");
+};
