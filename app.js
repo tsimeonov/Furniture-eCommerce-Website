75f1dc5589a9e7990d74198264cf920d7685c4ce
tsimeonov <tsimeonov12@gmail.com>
2024-07-13 14:28:02 +0200

    add the Product Filter

--- app.js
+++ app.js
@@ -48,3 +48,19 @@ const showElementOnScroll = function () {
 };
 
 addEvenOnElement(window, "scroll", showElementOnScroll);
+
+// Product Filter
+const filterBtns = document.querySelectorAll("[data-filter-btn]");
+const filterBox = document.querySelector("[data-filter]");
+
+let lastClickedFilterBtn = filterBtns[0];
+
+const filter = function () {
+  lastClickedFilterBtn.classList.remove("active");
+  this.classList.add("active");
+  lastClickedFilterBtn = this;
+
+  filterBox.setAttribute("data-filter", this.dataset.filterBtn);
+};
+
+addEvenOnElement(filterBtns, "click", filter);
