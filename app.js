a04a0c08dda9c924d27072117f5749f587f8051c
tsimeonov <tsimeonov12@gmail.com>
2024-08-01 19:09:22 +0200

    add the functionaly back to top

--- app.js
+++ app.js
@@ -35,15 +35,18 @@ const closeNavbar = function () {
 
 addEvenOnElement(navbarLinks, "click", closeNavbar);
 
-// Header active when window scroll down to 100px
+// Header & back to top active when window scroll down to 100px
 
 const header = document.querySelector("[data-header]");
+const backTopBtn  = document.querySelector("[data-back-top-btn]");
 
 const showElementOnScroll = function () {
   if (window.scrollY > 100) {
     header.classList.add("active");
+    backTopBtn.classList.add("active");
   } else {
     header.classList.remove("active");
+    backTopBtn.classList.remove("active");
   }
 };
 
