5e98af7affd2b3527fa9e445d2d78e850c8f463b
tsimeonov <tsimeonov12@gmail.com>
2025-06-07 16:42:43 +0200

    addEventOnElem

---
+++ app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// ADD EVENT ON ELEMENT
+
+const addEventOnElem = function (elem, type, callback) {
+	if (elem.length > 1) {
+		for (let i = 0; i < elem.length; i++) {
+			elem[i].addEventListener(type, callback);
+		}
+	} else {
+		elem.addEventListener(type, callback);
+	}
+};
