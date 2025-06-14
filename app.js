14ce4770751e830cfdb5293131e53f3edb2a3e5c
tsimeonov <tsimeonov12@gmail.com>
2025-06-14 19:55:29 +0200

    updawte js

--- app.js
+++ app.js
@@ -48,3 +48,20 @@ const showElemOnScroll = function () {
 };
 
 addEventOnElem(window, 'scroll', showElemOnScroll);
+
+// PRODUCT FILTER
+
+const filterBtns = document.querySelectorAll('[data-filter-btn]');
+const filterBox = document.querySelector('[data-filter]');
+
+let lastClickedFilterBtn = filterBtns[0];
+
+const filter = function () {
+	lastClickedFilterBtn.classList.remove('active');
+	this.classList.add('active');
+	lastClickedFilterBtn = this;
+
+	filterBox.setAttribute('data-filter', this.dataset.filterBtn);
+};
+
+addEventOnElem(filterBtns, 'click', filter);
