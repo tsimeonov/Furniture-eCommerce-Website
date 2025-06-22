53e86a7fc664c0e4e53556b75b00642cd13c5181
tsimeonov <tsimeonov12@gmail.com>
2025-06-22 17:44:03 +0200

    update js

--- app.js
+++ app.js
@@ -35,15 +35,18 @@ const closeNavbar = function () {
 
 addEventOnElem(navbarLinks, 'click', closeNavbar);
 
-// HEADER ACTIVE WHEN WINDOW SCROLL DOWN TO 100px
+// HEADER $ BACK TOP BTN ACTIVE WHEN WINDOW SCROLL DOWN TO 100px
 
 const header = document.querySelector('[data-header]');
+const backTopBtn = document.querySelector('[data-back-top-btn]');
 
 const showElemOnScroll = function () {
 	if (window.scrollY > 100) {
 		header.classList.add('active');
+		backTopBtn.classList.add('active');
 	} else {
 		header.classList.remove('active');
+		backTopBtn.classList.remove('active');
 	}
 };
 
